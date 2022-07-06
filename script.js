728c34a0ae12cf4cbd12fab0d6936abfc6760fa1
Alejandra Sánchez <ing.alejandrasanchezv@gmail.com>
2022-07-06 11:25:58 -0500

    Constructor functions update

--- script.js
+++ script.js
@@ -1,49 +1,13 @@
-const hero = {
-  id: "hero",
-  name: "Wizard",
-  avatar: "images/wizard.png",
-  health: 60,
-  diceRoll: [3, 1, 4],
-  diceCount: 3,
-};
+import characterData from "./data.js";
+import Character from "./Character.js";
 
-const monster = {
-  id: "monster",
-  name: "Orc",
-  avatar: "images/orc.png",
-  health: 10,
-  diceRoll: [2],
-  diceCount: 3,
-};
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
 
-function getDiceRollArray(diceCount) {
-  return new Array(diceCount).fill(0).map(function () {
-    return Math.floor(Math.random() * 6) + 1;
-  });
+function render() {
+  document.getElementById(wizard.elementId).innerHTML =
+    wizard.getCharacterHtml();
+  document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
 }
 
-function getDiceHtml(diceCount) {
-  return getDiceRollArray(diceCount)
-    .map(function (num) {
-      return `<div class="dice">${num}</div>`;
-    })
-    .join("");
-}
-
-function renderCharacter(data) {
-  const { id, name, avatar, health, diceCount } = data;
-
-  const diceHtml = getDiceHtml(diceCount);
-
-  document.getElementById(id).innerHTML = `<div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}" />
-        <div class="health">health: <b> ${health} </b></div>
-        <div class="dice-container">
-            ${diceHtml}
-        </div>
-    </div>`;
-}
-
-renderCharacter(hero);
-renderCharacter(monster);
+render();
